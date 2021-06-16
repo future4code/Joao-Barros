65e57ad5f5a4d0a79d34568181fdac9ea600842a
João Pedro Correia Barros <jpedrocbarros@gmail.com>
2021-06-16 14:08:53 -0300

    strings-arrays

---
+++ semana1/strings-arrays/index.js
@@ -0,0 +1,64 @@
+//_________Exercícios de interpretação de código________
+
+// 1
+
+// a. undefined
+// b. null
+// c. 11
+// d. 3
+// e. [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// f. 9
+
+// 2
+
+// SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+// _____________Exercícios de escrita de código___________
+
+// 1
+
+// const nome = prompt("Digite seu nome:")
+// const email = prompt("Digite seu email:")
+// console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`)
+
+// // 2
+
+// let comidas = ['arroz', 'feijao', 'batata', 'salgadinho', 'hamburguer']
+// console.log(comidas)
+// console.log("Essas são as minhas comidas preferidas: ")
+// console.log(comidas[0])
+// console.log(comidas[1])
+// console.log(comidas[2])
+// console.log(comidas[3])
+// console.log(comidas[4])
+
+// const comidaUser = prompt("Digite sua comida preferida:")
+// comidas[1] = comidaUser
+// console.log(comidas)
+
+// 3
+
+// const listaDeTarefas = []
+// const tarefa1 = prompt("Digite sua primeira tarefa:")
+// const tarefa2 = prompt("Digite sua segunda tarefa:")
+// const tarefa3 = prompt("Digite sua terceira tarefa:")
+// listaDeTarefas.push(tarefa1, tarefa2, tarefa3)
+// console.log(listaDeTarefas)
+
+// const indice = prompt("Digite um numero de 1 a 3:")
+// listaDeTarefas.splice(indice -1)
+// console.log(listaDeTarefas)
+
+// _______________________DESAFIOS_________________________
+
+// 1
+
+// const fraseUser = prompt("Escreva uma frase:")
+// const arrayFrase = fraseUser.split(' ')
+// console.log(arrayFrase)
+
+// 2
+
+const array2 = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+
+console.log(array2.indexOf('Abacaxi'))
