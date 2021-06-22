50c91b0d246ca534e570791dac0dc61e0f662aa8
João Pedro Correia Barros <jpedrocbarros@gmail.com>
2021-06-22 16:50:09 -0300

    condicionais

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,34 @@
+// 1 - a
+// O codigo verifica se o numero eh par.
+
+// b
+// Para os numeros pares.
+
+// c
+// impares
+
+// 2 - a
+// verificar qual eh a fruta e passar o preco.
+
+// b
+// O preço da fruta maca é R$ 2.25.
+
+// c
+// imprimiria o preco errado de 5 porque continuaria ate o default, esquecendo o preco de cima.
+
+// 3 - a
+// recebendo um numero do user.
+
+// b
+// se fosse 10 ele imprimiria as duas mensagems. se fosse -10 nao imprimiria nada.
+
+// c
+// havera um erro quando 
+const numero = Number(prompt("Digite o primeiro número."))
+
+if(numero > 0) {
+  console.log("Esse número passou no teste")
+	let mensagem = "Essa mensagem é secreta!!!"
+}
+
+console.log(mensagem)
