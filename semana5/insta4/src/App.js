c4aef24ccd082fb22049ac02203ee24d66b19b72
João Pedro Correia Barros <jpedrocbarros@gmail.com>
2021-07-15 16:41:50 -0300

    aula 20

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -10,24 +10,78 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+
+  state = {
+    arr: [
+      {
+        nome: 'paulinha',
+        fotoUser: 'https://picsum.photos/50/50?a=1',
+        fotoPost: 'https://picsum.photos/200/150?a=1'
+      },
+      {
+        nome: 'joao',
+        fotoUser: 'https://picsum.photos/50/50?a=2',
+        fotoPost: 'https://picsum.photos/200/150?a=2'
+      },
+      {
+        nome: 'pedro',
+        fotoUser: 'https://picsum.photos/50/50?a=3',
+        fotoPost: 'https://picsum.photos/200/150?a=3'
+      }
+    ],
+    valorInputFoto: "",
+    valorInputLegenda: "",
+    valorInputTexto: ""
+  }
+
+  adicionaPost = (arr) => {
+    const novoPost = {
+      valorInputFoto: this.state.valorInputFoto,
+      valorInputLegenda: this.state.valorInputLegenda,
+      valorInputTexto: this.state.valorInputTexto,
+    };
+
+    arr.push(novoPost);
+  }
+
+  onChangeInputFoto = (event) => {
+    this.setState({ valorInputFoto: event.target.value });
+  };
+
+  onChangeInputLegenda = (event) => {
+    this.setState({ valorInputLegenda: event.target.value });
+  };
+
+  onChangeInputTexto = (event) => {
+    this.setState({ valorInputTexto: event.target.value });
+  };
+
   render() {
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50?a=1'}
-          fotoPost={'https://picsum.photos/200/150?a=1'}
-        />
-        <Post
-          nomeUsuario={'joao'}
-          fotoUsuario={'https://picsum.photos/50/50?a=2'}
-          fotoPost={'https://picsum.photos/200/150?a=2'}
-        />
-        <Post
-          nomeUsuario={'pedro'}
-          fotoUsuario={'https://picsum.photos/50/50?a=3'}
-          fotoPost={'https://picsum.photos/200/150?a=3'}
-        />
+        <input
+            value={this.state.valorInputFoto}
+            onChange={this.onChangeInputFoto}
+            placeholder={"Link da foto"}
+          />
+          <input
+            value={this.state.valorInputLegenda}
+            onChange={this.onChangeInputLegenda}
+            placeholder={"Legenda"}
+          />
+          <input
+            value={this.state.valorInputTexto}
+            onChange={this.onChangeInputTexto}
+            placeholder={"Texto"}
+          />
+          <button onClick={this.adicionaPost}>Postar</button>
+        {this.state.arr.map(({ nome, fotoUser, fotoPost }) => (
+          <Post
+            nomeUsuario={nome}
+            fotoUsuario={fotoUser}
+            fotoPost={fotoPost}
+          />
+        ))}
       </MainContainer>
     );
   }
