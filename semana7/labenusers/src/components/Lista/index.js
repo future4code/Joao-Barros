8ea4fdf0c4b78751fc7122bf6f3e6d85295586df
João Pedro Correia Barros <jpedrocbarros@gmail.com>
2021-07-28 15:37:34 -0300

    desafio e estilo

--- semana7/labenusers/src/components/Lista/index.js
+++ semana7/labenusers/src/components/Lista/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const CardUsuario = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+  margin: 10px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users`;
 
@@ -11,7 +20,7 @@ const headers = {
 
 class Lista extends React.Component {
   state = {
-    usersList: []
+    usersList: [],
   };
 
   componentDidMount() {
@@ -19,23 +28,75 @@ class Lista extends React.Component {
   }
 
   getAllUsers = () => {
-
     axios
       .get(url, headers)
       .then((res) => {
-        this.setState({usersList: res.data})
+        this.setState({ usersList: res.data });
       })
       .catch((err) => {
-        alert(err)
+        alert(err);
+      });
+  };
+
+  deleteUser = (id) => {
+    const pergunta = prompt("Tem certeza que deseja deletar?");
+    if (
+      pergunta === "sim" ||
+      pergunta === "SIM" ||
+      pergunta === "Sim" ||
+      pergunta === "s" ||
+      pergunta === "S"
+    ) {
+      const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`;
+      axios
+        .delete(url, {
+          headers: {
+            Authorization: "joao-correia-lovelace",
+          },
+        })
+        .then(() => {
+          alert("Usuário deletado com sucesso!");
+          this.getAllUsers();
+        })
+        .catch(() => {
+          alert("Ocorreu um erro, tente novamente.");
+        });
+    } else {
+      alert("Tudo bem então, pai, c que manda.");
+    }
+  };
+
+  getUser = (id) => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`;
+    axios
+      .get(url, {
+        headers: {
+          Authorization: "joao-correia-lovelace",
+        },
+      })
+      .then((res) => {
+        alert(`Usuário: ${res.data.name} - Email: ${res.data.email}`);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Ocorreu um erro, tente novamente.");
       });
   };
 
   render() {
     const usersListComponent = this.state.usersList.map(({ name, id }) => (
-      <p key={id}>{name}</p>
-    ))
+      <CardUsuario key={id} onClick={() => this.getUser(id)}>
+        {name}
+        <button onClick={() => this.deleteUser(id)}>Excluir</button>
+      </CardUsuario>
+    ));
 
-    return <div>{usersListComponent}</div>
+    return (
+      <div>
+        <h3 style={{ textAlign: "center" }}>Lista de Usuários</h3>
+        {usersListComponent}
+      </div>
+    );
   }
 }
 
