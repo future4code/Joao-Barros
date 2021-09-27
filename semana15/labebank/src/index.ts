545ec19dcb0f46179e499f4b46fef23183be4bfd
João Pedro Correia Barros <jpedrocbarros@gmail.com>
2021-09-27 20:46:09 -0300

    adicionando um arquivo gitignore a um dos projetos

--- semana15/labebank/src/index.ts
+++ semana15/labebank/src/index.ts
@@ -12,8 +12,8 @@ type User = {
   name: string;
   cpf: number;
   age: number;
-  // balance: number;
-  // extract: Spending[];
+  balance?: number;
+  statement?: Spending[];
 };
 
 type Spending = {
@@ -29,35 +29,66 @@ type Transaction = {
   value: number;
 };
 
-const users: object[] = [];
+const users: object[] = [
+  {
+    id: 1,
+    name: "Joao",
+    cpf: 12345678912,
+    age: 28,
+    balance: 100,
+    statement: [
+      {
+        name: "Joao",
+        cpf: 12345678912,
+        date: 1632511504599,
+        value: 50
+      }
+    ]
+  }
+];
 
 app.post("/users", (req: Request, res: Response) => {
   try {
-    const { id, name, cpf, age } = req.body;
+    const { name, cpf, age } = req.body;
     if (age < 18) {
-      throw new Error("User must have more than 18 years.")
+      throw new Error("User must have more than 18 years.");
     }
-    const createUser: User | undefined = {
-      id, name, cpf, age
-    }
-    users.push(createUser)
-    return res.send(createUser)
+    const createUser: User = {
+      id: Date.now(),
+      name,
+      cpf,
+      age,
+      balance: 0,
+    };
+    users.push(createUser);
+    return res.send(createUser);
   } catch (err: any) {
-    return res.status(400).send({ message: err.message })
+    return res.status(400).send({ message: err.message });
   }
 });
 
 app.get("/users", (req: Request, res: Response) => {
   try {
     if (!res) {
-      throw new Error("Can not find users.")
+      throw new Error("Can not find users.");
     }
-    const usersList = users.map((user) => user)
-    return usersList
+    const usersList = users.map((user) => user);
+    res.send(usersList);
   } catch (err: any) {
-    return res.status(404).send({ message: err.message })
+    return res.status(404).send({ message: err.message });
   }
-})
+});
+
+// app.get("/users/search", (req: Request, res: Response) => {
+//   try {
+//     const result = users.filter(
+//       user => user.name.includes(req.query.name as string)
+//   )
+//   } catch (err: any) {
+
+//   }
+// })
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
