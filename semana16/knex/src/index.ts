1587f6a9344286c455223dc1963ca094e9dfb052
João Pedro Correia Barros <jpedrocbarros@gmail.com>
2021-09-29 23:02:42 -0300

    knex

---
+++ semana16/knex/src/index.ts
@@ -0,0 +1,74 @@
+import { Request, Response } from "express";
+import connection from "./connection";
+import app from "./app";
+
+// Esse arquivo seria o index.ts
+
+const getActorById = async (id: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE id = '${id}'
+  `);
+
+  return result[0];
+};
+
+const searchActor = async (name: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE name = "${name}"
+  `);
+  return result[0][0];
+};
+
+const countActors = async (gender: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT COUNT(*) as count FROM Actor WHERE gender = "${gender}"
+  `);
+	// Só colocamos esse "as count" como apelido, para ficar mais fácil de pegar
+	// o valor no resultado!
+  const count = result[0][0].count;
+  return count;
+};
+
+countActors("male")
+  .then((result: any) => {
+    console.log(result);
+  })
+  .catch((err: any) => {
+    console.log(err);
+  });
+
+searchActor("Ingrid Guimarães")
+  .then((result: any) => {
+    console.log(result);
+  })
+  .catch((err: any) => {
+    console.log(err);
+  });
+
+// Assim a chamada funciona fora dos endpoints com .then()/.catch
+getActorById("001")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Assim a chamada funciona fora dos endpoints com await
+(async () => {
+  console.log(await getActorById("001"));
+})();
+
+// Ou então podemos chamá-la dentro de um endpoint
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    console.log(await getActorById(id));
+
+    res.end();
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).send("Unexpected error");
+  }
+}); // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
